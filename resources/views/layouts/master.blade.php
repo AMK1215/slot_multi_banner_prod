8acafca4abcbb97554c5d44e98e6c62d8316bfa3
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-17 12:45:40 +0630

    deposit request

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,6 +25,8 @@
     <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
     @yield('style')
 
 
