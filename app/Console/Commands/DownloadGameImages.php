21ef436694b34109158b761a3e4df22e74e8bf92
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-20 12:24:50 +0630

    download image

--- app/Console/Commands/DownloadGameImages.php
+++ app/Console/Commands/DownloadGameImages.php
@@ -30,7 +30,7 @@ class DownloadGameImages extends Command
     public function handle()
 {
     // Path to the JSON file
-    $jsonFilePath = base_path('database/seeders/data/PGSOFT.json');
+    $jsonFilePath = base_path('database/seeders/data/PragmaticPlayLiveCasino.json');
 
     // Check if the JSON file exists
     if (! File::exists($jsonFilePath)) {
@@ -51,7 +51,7 @@ class DownloadGameImages extends Command
     $games = $gamesData['Game']; // Adjusted for the correct JSON structure
 
     // Create directory to store images
-    $directoryPath = public_path('assets/img/game_list/pg_soft');
+    $directoryPath = public_path('assets/img/game_list/pp_live');
     if (! File::exists($directoryPath)) {
         File::makeDirectory($directoryPath, 0755, true);
     }
