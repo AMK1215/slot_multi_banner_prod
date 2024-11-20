529120ca5f4835a23f9cb2450417316feba8248f
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-20 12:31:31 +0630

    download

--- app/Console/Commands/DownloadGameImages.php
+++ app/Console/Commands/DownloadGameImages.php
@@ -30,7 +30,7 @@ class DownloadGameImages extends Command
     public function handle()
 {
     // Path to the JSON file
-    $jsonFilePath = base_path('database/seeders/data/FUNTA.json');
+    $jsonFilePath = base_path('database/seeders/data/HSW.json');
 
     // Check if the JSON file exists
     if (! File::exists($jsonFilePath)) {
@@ -51,7 +51,7 @@ class DownloadGameImages extends Command
     $games = $gamesData['Game']; // Adjusted for the correct JSON structure
 
     // Create directory to store images
-    $directoryPath = public_path('assets/img/game_list/funta');
+    $directoryPath = public_path('assets/img/game_list/hsw');
     if (! File::exists($directoryPath)) {
         File::makeDirectory($directoryPath, 0755, true);
     }
