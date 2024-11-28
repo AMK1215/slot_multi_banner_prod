68be2403f615ae119113badf529faf4876c96d21
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-28 08:39:53 +0630

    get result for visualize data

--- app/Http/Controllers/Api/V1/Monitor/DataVisualizationController.php
+++ app/Http/Controllers/Api/V1/Monitor/DataVisualizationController.php
@@ -69,7 +69,7 @@ public function VisualizeResult()
             )
                 ->join('users as players', 'results.user_id', '=', 'players.id')
                 ->join('users as agents', 'players.agent_id', '=', 'agents.id')
-                ->groupBy('players.name', 'agents.name', 'players.id'); // Pagination for efficient data handling
+                ->groupBy('players.name', 'agents.name', 'players.id')->get();
 
             return $this->success($report, 'Results data retrieved successfully.');
         } catch (\Exception $e) {
