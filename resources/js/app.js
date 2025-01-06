d6bdcc3d6c031dfc9a18be04b87ceca7fbea96e3
AMK1215 <aungmyokhaing1215@gmail.com>
2025-01-06 12:25:22 +0630

    notification

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,8 @@ import Pusher from "pusher-js";
 window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: "pusher",
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: process.env.PUSHER_APP_KEY,
+    cluster: process.env.PUSHER_APP_CLUSTER,
     forceTLS: true,
 });
 
