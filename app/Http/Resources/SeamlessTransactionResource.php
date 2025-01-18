3bc5659d0514ea3356c9d7e703f5bfa153571a0c
sophia <thangneimawi@hivelocity.co.jp>
2025-01-18 23:32:47 +0700

    fixed api issue

--- app/Http/Resources/SeamlessTransactionResource.php
+++ app/Http/Resources/SeamlessTransactionResource.php
@@ -18,7 +18,7 @@ class SeamlessTransactionResource extends JsonResource
         return [
             'from_date' => Carbon::parse($this->from_date)->timezone('Asia/Yangon')->format('Y-m-d H:i:s'),
             'to_date' => Carbon::parse($this->to_date)->timezone('Asia/Yangon')->format('Y-m-d H:i:s'),
-            'product' => $this->game_provide_name,
+            'product' => $this->provider_name,
             'total_count' => $this->total_count,
             'total_bet_amount' => number_format($this->total_bet_amount, 2),
             'total_transaction_amount' => number_format($this->total_net_amount, 2),
